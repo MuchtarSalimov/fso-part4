552f8de31c82ea378eecaa49b6f5a209258e0579
Muchtar Salimov <muchtarsalimov@gmail.com>
2024-11-14 16:02:10 -0500

    update with testing mongo reset route

---
+++ controllers/testing.js
@@ -0,0 +1,13 @@
+require('express-async-errors')
+const baseRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+baseRouter.post('/reset', async (request, response) => {
+  await Blog.deleteMany({})
+  await User.deleteMany({})
+
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
